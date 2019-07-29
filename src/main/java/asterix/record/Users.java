363d75a1b9a6e845db20b25dfd8c6f586b754c3a
Michael <lwhaywhu@gmail.com>
2019-07-30 02:08:55 +0800

    Add nested records.

--- src/main/java/asterix/record/Users.java
+++ src/main/java/asterix/record/Users.java
@@ -1,5 +1,7 @@
 package asterix.record;
 
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import com.alibaba.fastjson.JSONObject;
@@ -7,6 +9,14 @@ import com.alibaba.fastjson.JSONObject;
 public class Users {
     private final UUID id;
     private String name;
+    private List<Address> addresses;
+    private List<PhoneDetail> phones;
+    private List<EmailDetail> emails;
+    private Map<String, String> character;
+
+    public Users() {
+        this.id = UUID.randomUUID();
+    }
 
     public Users(UUID uuid, String name) {
         this.id = uuid;
@@ -28,6 +38,38 @@ public class Users {
 
     public String getName() {
         return name;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public List<Address> getAddresses() {
+        return this.addresses;
+    }
+
+    public List<PhoneDetail> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<PhoneDetail> phones) {
+        this.phones = phones;
+    }
+
+    public List<EmailDetail> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<EmailDetail> emails) {
+        this.emails = emails;
+    }
+
+    public Map<String, String> getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(Map<String, String> character) {
+        this.character = character;
     }
 
     public String toJSONString() {
--- src/test/java/asterix/record/DatasetsTest.java
+++ src/test/java/asterix/record/DatasetsTest.java
@@ -1,9 +1,60 @@
 package asterix.record;
 
+import com.alibaba.fastjson.JSONObject;
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DatasetsTest {
     public static void main(String[] args) {
-        Users user = new Users("张三");
-        System.out.println(user.toJSONString());
+        {
+            Users user = new Users("张三");
+            List<Address> addresses = new ArrayList<>();
+
+            Address address = new Address();
+            address.setState("湖北");
+            address.setCity("武汉");
+            address.setPostalCode("430072");
+            address.setAddress("珞瑜路129号");
+            addresses.add(address);
+
+            address = new Address();
+            address.setState("湖北");
+            address.setCity("武汉");
+            address.setPostalCode("430074");
+            address.setAddress("珞瑜路1037号");
+            addresses.add(address);
+
+            user.setAddresses(addresses);
+
+            List<PhoneDetail> phones = new ArrayList<>();
+            PhoneDetail phone = new PhoneDetail();
+            phone.setCatetory("联通");
+            phone.setPhone("18986211330");
+            phones.add(phone);
+
+            user.setPhones(phones);
+
+            List<EmailDetail> emails = new ArrayList<>();
+            EmailDetail email = new EmailDetail();
+            email.setCategory("校园邮箱");
+            email.setEmail("hello@hust.edu.cn");
+            emails.add(email);
+
+            user.setEmails(emails);
+
+            Map<String, String> character = new HashMap<>();
+            character.put("国家", "中国");
+            user.setCharacter(character);
+
+            System.out.println(user.toJSONString());
+
+            Users user1 = JSONObject.parseObject(user.toJSONString(), Users.class);
+            System.out.println(user1.toJSONString());
+        }
 
         Events events = new Events("心跳异常");
         System.out.println(events.toJSONString());

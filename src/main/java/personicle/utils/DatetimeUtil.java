51dcaa17bc7846cd81cf42b0ba19228ed54863cf
Michael <lwhaywhu@gmail.com>
2019-08-03 09:02:36 +0800

    Add timestamp functions.

--- src/main/java/personicle/utils/DatetimeUtil.java
+++ src/main/java/personicle/utils/DatetimeUtil.java
@@ -1,7 +1,9 @@
 package personicle.utils;
 
+import akka.actor.ProviderSelection;
 import org.apache.logging.log4j.core.util.Assert;
 
+import java.sql.Timestamp;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -9,10 +11,46 @@ import java.time.format.DateTimeFormatter;
 
 public class DatetimeUtil {
 
-    private static final DateTimeFormatter ftf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSSS");
+    private static final DateTimeFormatter ftfm = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS");
+    private static final DateTimeFormatter ftfs = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter ftfd = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
 
-    public static String convertTimeToString(Long timestamp) {
+    public static String convertTimeMilliSecondToString(Long timestamp) {
         Assert.isNonEmpty(timestamp);
-        return ftf.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault()));
+        return ftfm.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault()));
+    }
+
+    public static Long convertTimeMilliSecondToLong(String time) {
+        Assert.isNonEmpty(time);
+        LocalDateTime parse = LocalDateTime.parse(time, ftfm);
+        return LocalDateTime.from(parse).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+
+    public static String convertTimeDozenSecondToString(Long timestamp) {
+        Assert.isNonEmpty(timestamp);
+        return ftfd.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault()));
+    }
+
+    public static Timestamp convertTimeMilliSecondToTimeStamp(Long timestamp) {
+        return Timestamp.valueOf(convertTimeDozenSecondToString(timestamp));
+    }
+
+    public static LocalDateTime convertTimeMilliSecondToLocalDateTime(Long timestamp) {
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
+    }
+
+    public static String convertTimeSecondToString(Long timestamp) {
+        Assert.isNonEmpty(timestamp);
+        return ftfs.format(LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp), ZoneId.systemDefault()));
+    }
+
+    public static Long convertTimeSecondToLong(String time) {
+        Assert.isNonEmpty(time);
+        LocalDateTime parse = LocalDateTime.parse(time, ftfs);
+        return LocalDateTime.from(parse).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+
+    public static Timestamp convertTimeSecondToTimeStamp(Long timestamp) {
+        return Timestamp.valueOf(convertTimeSecondToString(timestamp));
     }
 }
--- src/test/java/personicle/utils/DatetimeTest.java
+++ src/test/java/personicle/utils/DatetimeTest.java
@@ -1,10 +1,19 @@
 package personicle.utils;
 
-import java.io.IOException;
 import java.time.Instant;
 
 public class DatetimeTest {
-    public static void main(String[] args) throws IOException {
-        System.out.println(DatetimeUtil.convertTimeToString(Instant.now().toEpochMilli()));
+    public static void main(String[] args) {
+        System.out.println("msTOstring: " + DatetimeUtil.convertTimeMilliSecondToString(Instant.now().toEpochMilli()));
+        System.out.println("msTOlong: " + DatetimeUtil
+                .convertTimeMilliSecondToLong(DatetimeUtil.convertTimeMilliSecondToString(Instant.now().toEpochMilli()))
+                + " " + System.currentTimeMillis());
+        System.out.println("sTOstamp: " + DatetimeUtil.convertTimeSecondToTimeStamp(Instant.now().getEpochSecond()));
+        System.out.println(DatetimeUtil
+                .convertTimeSecondToLong(DatetimeUtil.convertTimeSecondToString(Instant.now().getEpochSecond())) + " "
+                + System.currentTimeMillis());
+        System.out.println(DatetimeUtil.convertTimeMilliSecondToTimeStamp(Instant.now().toEpochMilli()));
+        System.out.println(
+                "sTOlocal: " + DatetimeUtil.convertTimeMilliSecondToLocalDateTime(Instant.now().toEpochMilli()));
     }
 }

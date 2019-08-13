9335b77d96dd5508fd3b2fd3a3cb82cd3acd34b4
lwhay <lwh@whu.edu.cn>
2019-08-14 01:02:26 +0800

    Batching query.

--- src/test/java/asterix/config/AsterixIndexnlQuery.java
+++ src/test/java/asterix/config/AsterixIndexnlQuery.java
@@ -1,18 +1,39 @@
 package asterix.config;
 
+import asterix.record.users.PersonicleUser;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class AsterixIndexnlQuery {
+    private static int TOTAL_SIZE = 1000000;
+    private static int BATCH_SIZE = 100;
+
     public static void main(String[] args) throws Exception {
-        AsterixConf conf = new AsterixConf("http://127.0.0.1:19002");
-        AsterixConn conn = new AsterixConn();
-        conf.setDataverse("PersonicleServer");
-        String indexnlQuery = "\nSELECT f.foodname as fn, count(*) as counter" + "\nFROM users u, FoodLog f"
-                + "\nWHERE u.userName = \"匡书鹏\"" + "\nAND u.userId /*+ indexnl */ = f.userId"
-                + "\ngroup by f.foodname\n" + "order by counter desc;";
-        conf.setBody(indexnlQuery);
-        System.out.println(conf.getBasicUrl());
-        System.out.println(conf.getUrl(AsterixConf.OpType.QUERY));
-        System.out.println(conf.getQuery());
-        String ret = conn.handleRequest(conf, AsterixConf.OpType.QUERY);
-        System.out.println(ret);
+        List<String> users = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("./pseudo_users.adm"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            PersonicleUser user = JSONObject.parseObject(line, PersonicleUser.class);
+            if (users.size() == TOTAL_SIZE) {
+                break;
+            }
+            users.add(user.getUserName());
+        }
+        Random rand = new Random();
+        for (int i = 0; i < BATCH_SIZE; i++) {
+            AsterixConf conf = new AsterixConf("http://127.0.0.1:19002");
+            AsterixConn conn = new AsterixConn();
+            conf.setDataverse("PersonicleServer");
+            String indexnlQuery = "\nSELECT f.foodname as fn, count(*) as counter" + "\nFROM users u, FoodLog f"
+                    + "\nWHERE u.userName = \"" + users.get(rand.nextInt(users.size())).trim() + "\""
+                    + "\nAND u.userId /*+ indexnl */ = f.userId" + "\ngroup by f.foodname\n" + "order by counter desc;";
+            conf.setBody(indexnlQuery);
+            conn.handleRequest(conf, AsterixConf.OpType.QUERY);
+        }
     }
 }
